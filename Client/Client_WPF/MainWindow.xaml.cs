ffc1d512bb29de15adac0564dffed0c10cc103bf
Duong Pham Thai <duong1090@gmail.com>
2020-04-14 13:18:02 +0700

    Create Client_WPF

---
+++ Client/Client_WPF/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            DialogLoading dialog = new DialogLoading();
+            Grid.SetRow(dialog, 1);
+            grdMain.Children.Add(dialog);
+            dialog.loading.IsOpen = true;
+            ForwardPage();
+            grdMain.Children.Remove(dialog);
+            
+        }
+
+        void ForwardPage()
+        {
+            
+            grdLogin.Visibility = Visibility.Hidden;
+            grdHome.Visibility = Visibility.Visible;
+        }
+    }
+}
